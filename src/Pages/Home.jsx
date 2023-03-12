2655e998da994e40fdde064fdec09aa0bab19fb7
Binod <binod@Binods-MacBook-Air.local>
2023-03-12 20:54:57 +0545

    second commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Banner from '../Components/Banner/Banner';
 import Services from '../Components/servic/Services';
 import Ta from '../Components/Tab/Tabs';
-import Testimonials from '../Components/TESTIMONIALS/TESTIMONIALS';
+import Testimonials from '../Components/Testimonials/Testimonials';
 import Footer from '../Components/Footer/Footer';
 import Visit from '../Components/Visit/Visit';
 
